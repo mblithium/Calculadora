280ff87d316c93cfdf790da927b1096f7987f1c5
Matheus Bastos <matheusmcy1@gmail.com>
2025-02-02 02:42:09 -0300

    Last try...

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
+export const prerender = true;
+
 const app = mount(App, {
   target: document.getElementById('app'),
 })
